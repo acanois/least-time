c5d123b558feb164fb360e41bd6298c423de903c
acanois <david.richter42@gmail.com>
2025-02-15 14:45:03 -0600

    osc works, but needs work

--- source/main.cpp
+++ source/main.cpp
@@ -15,62 +15,32 @@
 class MaxReceiver : public osc::OscPacketListener {
     protected:
     
-        virtual void ProcessMessage( const osc::ReceivedMessage& m, 
+        virtual void ProcessMessage( const osc::ReceivedMessage& message, 
                     const IpEndpointName& remoteEndpoint )
         {
             (void) remoteEndpoint; // suppress unused parameter warning
-    
-            try{
-                // example of parsing single messages. osc::OsckPacketListener
-                // handles the bundle traversal.
-                
-                if (m.AddressPattern() == "/test1" ) {
-                    // example #1 -- argument stream interface
-                    osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
-                    bool a1;
-                    osc::int32 a2;
-                    float a3;
-                    const char *a4;
-                    args >> a1 >> a2 >> a3 >> a4 >> osc::EndMessage;
-                    
-                    std::cout << "received '/test1' message with arguments: "
-                        << a1 << " " << a2 << " " << a3 << " " << a4 << "\n";
-                    
-                }else if (m.AddressPattern() == "/test2") {
-                    // example #2 -- argument iterator interface, supports
-                    // reflection for overloaded messages (eg you can call 
-                    // (*arg)->IsBool() to check if a bool was passed etc).
-                    osc::ReceivedMessage::const_iterator arg = m.ArgumentsBegin();
-                    bool a1 = (arg++)->AsBool();
-                    int a2 = (arg++)->AsInt32();
-                    float a3 = (arg++)->AsFloat();
-                    const char *a4 = (arg++)->AsString();
-                    if( arg != m.ArgumentsEnd() )
-                        throw osc::ExcessArgumentException();
-                    
-                    std::cout << "received '/test2' message with arguments: "
-                        << a1 << " " << a2 << " " << a3 << " " << a4 << "\n";
-                }
-            }catch( osc::Exception& e ){
-                // any parsing errors such as unexpected argument types, or 
-                // missing arguments get thrown as exceptions.
-                std::cout << "error while parsing message: "
-                    << m.AddressPattern() << ": " << e.what() << "\n";
-            }
+
+            std::cout << message.AddressPattern() << std::endl;
         }
     };
 
 int main() {
     SetTargetFPS(60);
-
+    std::cout << "running -------------" << std::endl;
     MaxReceiver maxReceiver;
 
-    std::unique_ptr<Scene> scene = std::make_unique<Scene>();
+    UdpListeningReceiveSocket s(
+            IpEndpointName(IpEndpointName::ANY_ADDRESS, PORT),
+            &maxReceiver);
 
-    while (!WindowShouldClose()) {
-        scene->showGrid();
-        scene->draw();
-    }
+    // std::unique_ptr<Scene> scene = std::make_unique<Scene>();
+
+    // while (!WindowShouldClose()) {
+    //     scene->showGrid();
+    //     scene->draw();
+    // }
+
+    s.RunUntilSigInt();
 
     return 0;
 }
